b007fb512601e66ce43b0f8028db56daee2f7bae
Addarsh <addarsh.cherokie@gmail.com>
2025-07-21 11:07:58 +0530

    Broke main into 2 scripts, one to index emails and another to process

---
+++ index_emails.py
@@ -0,0 +1,14 @@
+from services.gmail_service import GmailService
+from services.email_service import ListEmailsRequest
+from database.email_manager import EmailManager
+
+if __name__ == "__main__":
+    gs = GmailService()
+    response = gs.list(list_emails_req=ListEmailsRequest(senders=["sanaa.p@thoughtspot.com>"], max_results=2))
+    print(f"Got email results count: {response.count}")
+    for i, email in enumerate(response.emails):
+        print(f"\n\nEmail {i}:\n{email}")
+
+    email_mgr = EmailManager("emails.db")
+    email_mgr.insert(response.emails)
+    email_mgr.read(email_ids=[email.id for email in response.emails])
--- process_emails.py
+++ process_emails.py
@@ -1,18 +1,7 @@
-from services.gmail_service import GmailService
-from services.email_service import ListEmailsRequest
 from database.email_manager import EmailManager, FilterEmailsRequest
 
 if __name__ == "__main__":
-    # gs = GmailService()
-    # response = gs.list(list_emails_req=ListEmailsRequest(senders=["sanaa.p@thoughtspot.com>"], max_results=2))
-    # print(f"Got email results count: {response.count}")
-    # for i, email in enumerate(response.emails):
-    #     print(f"\n\nEmail {i}:\n{email}")
-
     email_mgr = EmailManager("emails.db")
-    # email_mgr.insert(response.emails)
-    # email_mgr.read(email_ids=[email.id for email in response.emails])
-
     filter_req = FilterEmailsRequest(
         filter=FilterEmailsRequest.RulesCollection(
             rules=[
